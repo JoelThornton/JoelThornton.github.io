bcebc09f1fbfb6048e0bab63f7596c722c963498
CammyFentreux <cameron1709@live.co.uk>
2019-12-11 19:30:21 +0000

    reformated index page

--- Opp.js
+++ Opp.js
@@ -1,6 +1,45 @@
 
-function createOpp(name, desc) {  // factory of opp
+function createOpp(name, desc, img_src, container=document.getElementById("opportunities")) {  // factory of opp. container: element that opportunity is slaved to
+  // create elements
+  const div = document.createElement("div")  // container div
+  const nameP = document.createElement("p")  // <p>: opp name
+  const descP = document.createElement("p")  // <p>: description
+  const img = document.createElement("img")
+
+  // populate elements
+  nameP.innerHTML = name + ":"
+  descP.innerHTML = desc
+  img.src = img_src
+
+  // set class
+  div.className = "opp"
+  nameP.className = "vis"
+  descP.className = "hid"
+  img.className = "hid"
+
+  // events
+  const show = () => {
+    descP.className = descP.className.replace("hid", "vis")
+    img.className = img.className.replace("hid", "vis")
+    div.onclick = hide
+  }
+  const hide = () => {
+    descP.className = descP.className.replace("vis", "hid")
+    img.className = img.className.replace("vis", "hid")
+    div.onclick = show
+  }
+  div.onclick = show
+
+  div.onmouseenter = () => {div.className += " highlight"}
+  div.onmouseleave = () => {div.className = div.className.replace(" highlight", "")}
+
+  // append to parents
+  div.appendChild(nameP)
+  div.appendChild(descP)
+  div.appendChild(img)
+  container.appendChild(div)
   return {
-    name, desc
+    nameP,
+    descP
   }
 }
--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const opps = []  // opportunities
 
-opps.push(createOpp("Hugh Baird: South Sefton Campus", "A sixth form college dedicated to A'level based qualifications."))
-opps.push(createOpp("Hugh Baird: Main Campus", "Aimed at practical BTEC qualifications, Hugh Baird is a large college with several other campuses."))
-opps.push(createOpp("Litherland Sports Park", "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."))
+opps.push(createOpp("Hugh Baird: South Sefton Campus", "A sixth form college dedicated to A'level based qualifications.", "Resources\\img\\HB_SSC.jpg"))
+opps.push(createOpp("Hugh Baird: Main Campus", "Aimed at practical BTEC qualifications, Hugh Baird is a large college with several other campuses.", "Resources\\img\\HB_MC.jpg"))
+opps.push(createOpp("Litherland Sports Park", "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians.",
+                    "Resources\\img\\LSP.jpg"))
 // opps.push(createOpp("", ""))
-const opportunities = document.getElementById("opportunities")
 for (let opp of opps) {
-  const div =  document.createElement("div")
-  const name = document.createElement("p")
-  const desc = document.createElement("p")
-  // name.style.
-  name.innerHTML = opp.name
-  desc.innerHTML = ": " + opp.desc
-  div.appendChild(name)
-  div.appendChild(desc)
-  opportunities.appendChild(div)
+
+
 
   console.log("pass");
 }
