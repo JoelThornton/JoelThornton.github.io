f7a291088aaf22ac05c4bf4fd394985553861689
CammyFentreux <cameron1709@live.co.uk>
2019-12-30 09:17:47 +0000

    Removed test opp name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const opps = []  // opportunities
 
-opps.push(createOpp("Hugh Baird:      sdasdaddsdad      South Sefton Campus", "Resources\\img\\HB_SSC.jpg"))  // , "A sixth form college dedicated to A'level based qualifications."
+opps.push(createOpp("Hugh Baird: South Sefton Campus", "Resources\\img\\HB_SSC.jpg"))  // , "A sixth form college dedicated to A'level based qualifications."
 opps.push(createOpp("Hugh Baird: Main Campus", "Resources\\img\\HB_MC.jpg"))  // , "Aimed at practical BTEC qualifications, Hugh Baird is a large college with several other campuses."
 opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
 opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
