9fceeecba6d51a590fbba185cbdcd1e379ed9e67
CammyFentreux <cameron1709@live.co.uk>
2019-12-30 08:50:04 +0000

    Tabulated opportunities

--- Opp.js
+++ Opp.js
@@ -1,48 +1,30 @@
 
-function createOpp(name, desc, img_src, container=document.getElementById("opportunities")) {  // factory of opp. container: element that opportunity is slaved to
+function createOpp(name, img_src, container=document.getElementById("opportunities")) {  // factory of opp. container: element that opportunity is slaved to
   // create elements
   const div = document.createElement("div")  // container div
   const nameP = document.createElement("p")  // <p>: opp name
-  const descP = document.createElement("p")  // <p>: description
   const img = document.createElement("img")
 
   // populate elements
-  nameP.innerHTML = name + ":"
-  descP.innerHTML = desc
+  nameP.innerHTML = name
   img.src = img_src
 
   // set class
   div.className = "opp"
   nameP.style.display = "block"
-  descP.style.display = "block"
   img.style.display = "block"
 
-  // events
-  const show = () => {  // display extra info
-    div.style.animationName = "enlarge"
-    div.style.animationDuration = "0.2s"
-    div.style.animationFillMode = "forwards"
-    div.onclick = hide
-  }
-  const hide = () => {  // hide extra info
-    div.style.animationName = "shrink"
-    div.style.animationDuration = "0.2s"
-    div.style.animationFillMode = "forwards"
-    div.onclick = show
-  }
-  div.onclick = show
   div.onmouseenter = () => {div.className += " highlight"}
   div.onmouseleave = () => {div.className = div.className.replace(" highlight", "")}
 
   // append to parents
   div.appendChild(nameP)
-  div.appendChild(descP)
   div.appendChild(img)
+  console.log(container);
   container.appendChild(div)
 
   // create and return opp object
   return {
     nameP,
-    descP
   }
 }
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const opps = []  // opportunities
 
-opps.push(createOpp("Hugh Baird: South Sefton Campus", "A sixth form college dedicated to A'level based qualifications.", "Resources\\img\\HB_SSC.jpg"))
-opps.push(createOpp("Hugh Baird: Main Campus", "Aimed at practical BTEC qualifications, Hugh Baird is a large college with several other campuses.", "Resources\\img\\HB_MC.jpg"))
-opps.push(createOpp("Litherland Sports Park", "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians.",
-                    "Resources\\img\\LSP.jpg"))
+opps.push(createOpp("Hugh Baird:      sdasdaddsdad      South Sefton Campus", "Resources\\img\\HB_SSC.jpg"))  // , "A sixth form college dedicated to A'level based qualifications."
+opps.push(createOpp("Hugh Baird: Main Campus", "Resources\\img\\HB_MC.jpg"))  // , "Aimed at practical BTEC qualifications, Hugh Baird is a large college with several other campuses."
+opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
+opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
+opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
+opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
+opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
 // opps.push(createOpp("", ""))
