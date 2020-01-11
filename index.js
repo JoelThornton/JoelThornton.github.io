9b1dfec1423dbd59e3652c7e4de62c8086899dc4
CammyFentreux <cameron1709@live.co.uk>
2020-01-11 14:50:52 +0000

    Refactored opps to use localStorage

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+
 const opps = []  // opportunities
 
-opps.push(createOpp("Hugh Baird: South Sefton Campus", "Resources\\img\\HB_SSC.jpg"))  // , "A sixth form college dedicated to A'level based qualifications."
-opps.push(createOpp("Hugh Baird: Main Campus", "Resources\\img\\HB_MC.jpg"))  // , "Aimed at practical BTEC qualifications, Hugh Baird is a large college with several other campuses."
-opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
-opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
-opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
-opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
-opps.push(createOpp("Litherland Sports Park", "Resources\\img\\LSP.jpg"))  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
-// opps.push(createOpp("", ""))
 
-for (let opp of opps) {
-  opp.display()
+for (let i=0; i<window.localStorage.length; i++) {
+  displayOpp(opp)
+}
+
+function displayOpp(name, img_src, container=document.getElementById("opportunities")) {
+  // create elements
+  const div = document.createElement("div")  // container div
+  const nameP = document.createElement("p")  // <p>: opp name
+  const img = document.createElement("img")
+
+  // populate elements
+  nameP.innerHTML = name
+  img.src = img_src
+
+  // set class
+  div.className = "opp"
+  nameP.style.display = "block"
+  img.style.display = "block"
+
+  // set events
+  div.onmouseenter = () => {div.className += " highlight"}
+  div.onmouseleave = () => {div.className = div.className.replace(" highlight", "")}
+
+  // append to parents
+  div.appendChild(nameP)
+  div.appendChild(img)
+  container.appendChild(div)
 }
