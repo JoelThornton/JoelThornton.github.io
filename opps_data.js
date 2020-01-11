63462f4604bbe79c83de315a59d33b2a1f182554
CammyFentreux <cameron1709@live.co.uk>
2020-01-11 16:05:02 +0000

    refactored opps_data.js

--- opps_data.js
+++ opps_data.js
@@ -1,4 +1,15 @@
 
-window.sessionStorage.setItem("Hugh Baird: South Sefton Campus", "Resources\\img\\HB_SSC.jpg")  // , "A sixth form college dedicated to A'level based qualifications."
-window.sessionStorage.setItem("Hugh Baird: Main Campus", "Resources\\img\\HB_MC.jpg")  // , "Aimed at practical BTEC qualifications, Hugh Baird is a large college with several other campuses."
-window.sessionStorage.setItem("Litherland Sports Park", "Resources\\img\\LSP.jpg")  // , "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians."
+opps = {
+  "Hugh Baird: South Sefton Campus" : {
+    img_src: "Resources\\img\\HB_SSC.jpg",
+    desc: "A sixth form college dedicated to A'level based qualifications.",
+  },
+  "Hugh Baird: Main Campus": {
+    img_src: "Resources\\img\\HB_MC.jpg",
+    desc: "Aimed at practical BTEC qualifications, Hugh Baird is a large college with several other campuses.",
+  },
+  "Litherland Sports Park": {
+    img_src: "Resources\\img\\LSP.jpg",
+    desc: "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians.",
+  },
+}
