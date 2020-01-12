54d5610a5bb1b418fe3e8124de791ec7dd88cce0
CammyFentreux <cameron1709@live.co.uk>
2020-01-12 06:08:39 +0000

    Added tags to opps

--- opps_data.js
+++ opps_data.js
@@ -3,13 +3,16 @@ opps = {
   "Hugh Baird: South Sefton Campus" : {
     img_src: "Resources\\img\\HB_SSC.jpg",
     desc: "A sixth form college dedicated to A'level based qualifications.",
+    tags: ["Education", "A-Level"]
   },
   "Hugh Baird: Main Campus": {
     img_src: "Resources\\img\\HB_MC.jpg",
     desc: "Aimed at practical BTEC qualifications, Hugh Baird is a large college with several other campuses.",
+    tags: ["Education", "BTEC"]
   },
   "Litherland Sports Park": {
     img_src: "Resources\\img\\LSP.jpg",
     desc: "This family sized gym is great for the local community who wish to get fit. It has olympic grade facilties, tested by real olympians.",
+    tags: ["Sport", "Exercise", "Gym"]
   },
 }
