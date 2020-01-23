0d00710f611dfe4042339f54c83a15a4507e7431
Cammy <cameron1709@live.co.uk>
2020-01-23 21:28:23 +0000

    converted opps to array

--- opp.js
+++ opp.js
@@ -1,12 +1,12 @@
 
 window.onload = function() {
   const url = document.location.href
-  const name = decodeURIComponent(url.split("?")[1].split("=")[1])
-  const opp = opps[name]
+  const index = decodeURIComponent(url.split("?")[1].split("=")[1])
+  const opp = opps[index]
 
   // populate page
-  document.title = "YAP - " + name
-  document.getElementById("name").innerHTML = name
+  document.title = "YAP - " + opp.name
+  document.getElementById("name").innerHTML = opp.name
   document.getElementById("img").src = opp.img_src
   document.getElementById("desc").innerHTML = opp.desc
 }
